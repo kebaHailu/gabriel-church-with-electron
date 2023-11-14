caddbd5580910ffecf01224e9f081a22b304601a
gebby <you@example.com>
2023-11-14 23:41:21 +0300

    try to solve validation

--- src/components/makelawian/makelawian.js
+++ src/components/makelawian/makelawian.js
@@ -1,13 +1,15 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../wetatoch/styles.css";
-import {Typography,Box, Grid,TextField } from "@mui/material";
-import Button from '@mui/material/Button';
-import {InputLabel,MenuItem, FormControl,Select } from "@mui/material";
-import { DatePicker } from '@mui/x-date-pickers';
+import { Typography, Box, Grid, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import { InputLabel, MenuItem, FormControl, Select } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
 import ProfilePictureUploader from "../ProfilePicture/ProfilePictureUploader";
 
 import InputMask from "react-input-mask";
 import { useFormik } from "formik";
+import { basicSchema } from "../../indexx";
+import { Formik, Form, Field } from "formik";
 
 const PhoneInput = (props) => {
   return (
@@ -21,6 +23,23 @@ const PhoneInput = (props) => {
 };
 
 export default function Makelawian() {
+  var { values, handleChange, handleBlur, handleSubmit, touched, errors } =
+    useFormik({
+      initialValues: {
+        fullName: "",
+        churchName: "",
+        churchFatherName: "",
+        churchFatherPhone: "",
+        address: "",
+        houseNumber: "",
+        phone1: "",
+        email: "",
+        educationName: "",
+        family:"",
+      },
+      validationSchema: basicSchema,
+      onSubmit: (value) => {},
+    });
   function useInput(initialValue) {
     const [state, setState] = useState(initialValue);
     const handleChange = (event) => {
@@ -28,7 +47,7 @@ export default function Makelawian() {
     };
     return [state, handleChange];
   }
-  const [sex, handleChange] = useInput("");
+  var [sex, handleChange] = useInput("");
   const [marriage, marriageChange] = useInput("");
   const [education, educationChange] = useInput("");
   const [cheducation, cheducationChange] = useInput("");
@@ -39,7 +58,7 @@ export default function Makelawian() {
     <div className="title">
       <Typography variant="h4">የማእከላዊያን አባላት ቅጽ መሙያ</Typography>
 
-      <form id="wetatoch-form">
+      <form onSubmit={handleSubmit} id="wetatoch-form">
         <div className="personal-info">
           <Box
             sx={{
@@ -48,165 +67,227 @@ export default function Makelawian() {
           >
             <Typography variant="h5">የአባሉ መረጃ</Typography>
             <ProfilePictureUploader section="personal-info" />
-
-            <TextField
-              className="personal-info-input"
-              id="fullName"
-              name="fullName"
-              label="ሙሉ ስም"
-              required={true}
-              variant="standard"
-              helperText="የሙሉ ስም ያስገቡ"
-            />
-            <DatePicker label="የትውልድ ዘመን፡" />
-
-            <TextField
-              className="personal-info-input"
-              name="churchName"
-              required
-              label="የክርስትና ስም"
-              variant="standard"
-            />
-
-            <FormControl className="formcontrol">
-              <InputLabel id="sex">ፆታ</InputLabel>
-              <Select value={sex} label="sex" onChange={handleChange}>
-                <MenuItem value={10}>ወንድ</MenuItem>
-                <MenuItem value={20}>ሴት</MenuItem>
-              </Select>
-            </FormControl>
-
-            <TextField
-              className="personal-info-input"
-              name="churchFatherName"
-              label="የንስሐ አባት ስም"
-              variant="standard"
-            />
-            <TextField
-              className="personal-info-input"
-              name="churchFatherPhone"
-              label="የንስሕ አባት ስ.ቁ"
-              variant="standard"
-              InputProps={{
-                inputComponent: PhoneInput,
-              }}
-            />
-
-            <TextField
-              className="personal-info-input"
-              name="address"
-              label="የመኖሪያ አድራሻ"
-              variant="standard"
-            />
-            <FormControl className="formcontrol">
-              <InputLabel>የቤተክርስቲያን አገልግሎት</InputLabel>
-              <Select
-                value={churchContrbution}
-                label="churchContrbution"
-                onChange={churchContrbutionChange}
-              >
-                <MenuItem value={1}>ከበሮ መምታት</MenuItem>
-                <MenuItem value={2}>ዜማ መድረስ</MenuItem>
-                <MenuItem value={3}>የዝማሬ መሣሪያዎች ችሎታ</MenuItem>
-                <MenuItem value={4}>ሥነጽሁፍ ድርሰት</MenuItem>
-                <MenuItem value={5}>በክህነት</MenuItem>
-              </Select>
-            </FormControl>
-
-            <TextField
-              className="personal-info-input"
-              name="houseNumber"
-              label="የቤት ቁጥር"
-              variant="standard"
-            />
-
-            <FormControl className="formcontrol">
-              <InputLabel id="demo-simple-select-label">
-                የተከታተሉት የመንፈሳዊ ት/ት
-              </InputLabel>
-              <Select
-                value={cheducation}
-                label="cheducation"
-                onChange={cheducationChange}
-              >
-                {" "}
-                <MenuItem value={0}>አልተከታተልኩም</MenuItem>
-                <MenuItem value={1}>ቀዳማይ</MenuItem>
-                <MenuItem value={2}>ሳልሳይ</MenuItem>
-                <MenuItem value={3}>ካልዓይ</MenuItem>
-                <MenuItem value={4}>ራብዓይ</MenuItem>
-              </Select>
-            </FormControl>
-
-            <TextField
-              className="personal-info-input"
-              name="phone1"
-              label="የሞባይል ስልክ ቁጥር "
-              variant="standard"
-              InputProps={{
-                inputComponent: PhoneInput,
-              }}
-            />
-
-            <FormControl className="formcontrol">
-              <InputLabel id="demo-simple-select-label">
-                በሌላ ሰ/ት/ቤት ያገለገሉበት ዘርፍ?
-              </InputLabel>
-              <Select
-                value={otherchurch}
-                label="otherchurch"
-                onChange={otherchurchChange}
-              >
-                <MenuItem value={1}>አገልግዬ አላውቅም</MenuItem>
-                <MenuItem value={2}>በአመራር</MenuItem>
-                <MenuItem value={3}>በክህነት</MenuItem>
-                <MenuItem value={4}>በአባልነት</MenuItem>
-              </Select>
-            </FormControl>
-
-            <TextField
-              className="personal-info-input"
-              name="phone2"
-              label="ተጨማሪ ስልክ ቁጥር "
-              variant="standard"
-              InputProps={{
-                inputComponent: PhoneInput,
-              }}
-            />
-            <TextField
-              className="personal-info-input"
-              name="email"
-              label="ኢሜይል"
-              variant="standard"
-            />
-            <FormControl className="formcontrol">
-              <InputLabel id="demo-simple-select-label">የት/ት ደረጃ</InputLabel>
-              <Select
-                value={education}
-                label="education"
-                onChange={educationChange}
-              >
-                <MenuItem value={4}>ዲፕሎማ</MenuItem>
-                <MenuItem value={4}>መሰናዶ</MenuItem>
-                <MenuItem value={5}>2ኛ ደርጃ</MenuItem>
-                <MenuItem value={6}>1ኛ ደርጃ</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField
-              className="personal-info-input"
-              name="educationName"
-              label="የትምህርት ቤቱ ስም"
-              variant="standard"
-            />
-
-            <TextField
-              className="family-unique"
-              name="family"
-              fullWidth
-              multiline
-              rows={4}
-              label="የቤተሰብ አባላት ከስልክ ቁጥር ጋር"
-            />
+            <Formik>
+              <Form>
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  id="fullName"
+                  name="fullName"
+                  // type='fullName'
+                  label="ሙሉ ስም"
+                  variant="standard"
+                  value={values.fullName }
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.fullName && touched.fullName && (
+                  <p className="errors">{errors.fullName}</p>
+                )}
+                <DatePicker label="የትውልድ ዘመን፡" />
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="churchName"
+                  required
+                  label="የክርስትና ስም"
+                  variant="standard"
+                  value={values.churchName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.churchName && touched.churchName && (
+                  <p className="errors">{errors.churchName}</p>
+                )}
+                <FormControl className="formcontrol">
+                  <InputLabel id="sex">ፆታ</InputLabel>
+                  <Select value={sex} label="sex" onChange={handleChange}>
+                    <MenuItem value={10}>ወንድ</MenuItem>
+                    <MenuItem value={20}>ሴት</MenuItem>
+                  </Select>
+                </FormControl>
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="churchFatherName"
+                  label="የንስሐ አባት ስም"
+                  variant="standard"
+                  value={values.churchFatherName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.churchFatherName && touched.churchFatherName && (
+                  <p className="errors">{errors.churchFatherName}</p>
+                )}
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="churchFatherPhone"
+                  label="የንስሕ አባት ስ.ቁ"
+                  variant="standard"
+                  value={values.churchFatherPhone}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.churchFatherPhone && touched.churchFatherPhone && (
+                  <p className="errors-right">{errors.churchFatherPhone}</p>
+                )}
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="address"
+                  label="የመኖሪያ አድራሻ"
+                  variant="standard"
+                  value={values.address}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.address && touched.address && (
+                  <p className="errors">{errors.address}</p>
+                )}
+                <FormControl className="formcontrol">
+                  <InputLabel>የቤተክርስቲያን አገልግሎት</InputLabel>
+                  <Select
+                    value={churchContrbution}
+                    label="churchContrbution"
+                    onChange={churchContrbutionChange}
+                  >
+                    <MenuItem value={1}>ከበሮ መምታት</MenuItem>
+                    <MenuItem value={2}>ዜማ መድረስ</MenuItem>
+                    <MenuItem value={3}>የዝማሬ መሣሪያዎች ችሎታ</MenuItem>
+                    <MenuItem value={4}>ሥነጽሁፍ ድርሰት</MenuItem>
+                    <MenuItem value={5}>በክህነት</MenuItem>
+                  </Select>
+                </FormControl>
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="houseNumber"
+                  label="የቤት ቁጥር"
+                  variant="standard"
+                  value={values.houseNumber}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.houseNumber && touched.houseNumber && (
+                  <p className="errors">{errors.houseNumber}</p>
+                )}
+                <FormControl className="formcontrol">
+                  <InputLabel id="demo-simple-select-label">
+                    የተከታተሉት የመንፈሳዊ ት/ት
+                  </InputLabel>
+                  <Select
+                    value={cheducation}
+                    label="cheducation"
+                    onChange={cheducationChange}
+                  >
+                    {" "}
+                    <MenuItem value={0}>አልተከታተልኩም</MenuItem>
+                    <MenuItem value={1}>ቀዳማይ</MenuItem>
+                    <MenuItem value={2}>ሳልሳይ</MenuItem>
+                    <MenuItem value={3}>ካልዓይ</MenuItem>
+                    <MenuItem value={4}>ራብዓይ</MenuItem>
+                  </Select>
+                </FormControl>
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="phone1"
+                  label="የሞባይል ስልክ ቁጥር "
+                  variant="standard"
+                  value={values.phone1}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.phone1 && touched.phone1 && (
+                  <p className="errors">{errors.phone1}</p>
+                )}
+                <FormControl className="formcontrol">
+                  <InputLabel id="demo-simple-select-label">
+                    በሌላ ሰ/ት/ቤት ያገለገሉበት ዘርፍ?
+                  </InputLabel>
+                  <Select
+                    value={otherchurch}
+                    label="otherchurch"
+                    onChange={otherchurchChange}
+                  >
+                    <MenuItem value={1}>አገልግዬ አላውቅም</MenuItem>
+                    <MenuItem value={2}>በአመራር</MenuItem>
+                    <MenuItem value={3}>በክህነት</MenuItem>
+                    <MenuItem value={4}>በአባልነት</MenuItem>
+                  </Select>
+                </FormControl>
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="phone2"
+                  label="ተጨማሪ ስልክ ቁጥር "
+                  variant="standard"
+                  value={values.phone1}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.phone1 && touched.phone1 && (
+                  <p className="errors">{errors.phone1}</p>
+                )}
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="email"
+                  label="ኢሜይል"
+                  variant="standard"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.email && touched.email && (
+                  <p className="errors-right">{errors.email}</p>
+                )}
+                <FormControl className="formcontrol">
+                  <InputLabel id="demo-simple-select-label">
+                    የት/ት ደረጃ
+                  </InputLabel>
+                  <Select
+                    value={education}
+                    label="education"
+                    onChange={educationChange}
+                  >
+                    <MenuItem value={4}>ዲፕሎማ</MenuItem>
+                    <MenuItem value={4}>መሰናዶ</MenuItem>
+                    <MenuItem value={5}>2ኛ ደርጃ</MenuItem>
+                    <MenuItem value={6}>1ኛ ደርጃ</MenuItem>
+                  </Select>
+                </FormControl>
+                <Field
+                  as={TextField}
+                  className="personal-info-input"
+                  name="educationName"
+                  label="የትምህርት ቤቱ ስም"
+                  variant="standard"
+                  value={values.educationName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.educationName && touched.educationName && (
+                  <p className="errors-right">{errors.educationName}</p>
+                )}
+                <Field
+                  as={TextField}
+                  className="family-unique"
+                  name="family"
+                  fullWidth
+                  multiline
+                  rows={4}
+                  label="የቤተሰብ አባላት ከስልክ ቁጥር ጋር"
+                  value={values.family}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.family && touched.family && (
+                  <p className="errors-right">{errors.family}</p>
+                )}
+              </Form>
+            </Formik>
           </Box>
         </div>
 
