da94711c0235798cfce42d956e497c2beeb897e2
gebby123 <tefayegebrehiwot123@gmail.com>
2023-11-29 19:59:28 +0300

    all mereja files css style changed bsed on their size

--- src/components/Mereja/FrewochData.jsx
+++ src/components/Mereja/FrewochData.jsx
@@ -32,79 +32,89 @@ function FrewochData() {
     }
   };
         return (
-                <div className="row_posters">
-                  <span className='st'>የፍሬ አባላት መረጃ</span>
-                <table>
-                    <thead className="title-row">
-                        <tr>
-                            <th> ምስል </th>
-                            <th className="s-2">ሙሉ ስም</th>
-                            <th>የትውልድ ዝመን</th>
-                            <th >የክርስትና ስም</th>
-                            <th> ፆታ</th>
-                            <th >የመኖሪያ አድሪሻ</th>
-                            <th className='s-4'>የቤት ቁጥር</th>
-                            <th className="s-3">ዋና ስልክ ቁጥር</th>
-                            <th className="s-3">ተጨማሪ ስልክ ቁጥር</th>
-                            <th className="s-1">ኢሜል</th>
-                            <th>የትዳር ሁኔታ</th>
-                            <th>የስራ ሁኔታ</th>
-                            <th>የት/ት ደረጃ</th>
-                            <th>የተሰማሩበት የስራ ዘርፍ</th>
-                            <th >የቴሌግራም  ስም</th>
-                            <th >የቤተሰብ አባላት ከስልክ ቁጥር ጋር</th>
-                            <th >የቤተክርስቲያን አገልግሎት</th>
-                            <th >የንስሐ አባት ስም</th>
-                            <th>የንስሐ አባት ስ.ቁ</th>
-                            <th>የተከታተሉት የመንፈሳዊ ት/ት</th>
-                            <th className='s-4'>በሌላ ሰ/ት/ቤት ያገለገሉበት ዘርፍ</th>
-                            <th>የክፍሉ ተጠሪ ስም</th>
-                            <th>የተመዘገበበት ቀን</th>
-                        
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((row, index) => (
-                            <tr
-                                key={row.doc._id}
-                                className={index % 2 === 0 ? "row-even" : "row-odd"}>
-                              <td>{row.doc.ProfileImage ? <img src={URL.createObjectURL(row.doc.ProfileImage)} alt="Profile" style={{ width: 100, height: 100 }} /> : "N/A"}</td>
-                                <td>{row.doc.fullName || "N/A"}</td>
-                                <td>{row.doc.Birthdate || "N/A"}</td>
-                                <td>{row.doc.churchName || "N/A"}</td>
-                                <td>{row.doc.sex || "N/A"}</td>
-                                <td>{row.doc.address || "N/A"}</td>
-                                <td>{row.doc.houseNumber || "N/A"}</td>
-                                <td>{row.doc.phone1 || "N/A"}</td>
-                                <td>{row.doc.phone2 || "N/A"}</td>
-                                <td>{row.doc.email || "N/A"}</td>
-                                <td>{row.doc.marriage || "N/A"}</td>
-                                <td>{row.doc.occupation || "N/A"}</td>
-                                <td>{row.doc.education || "N/A"}</td>
-                                <td>{row.doc.workPlace || "N/A"}</td>
-                                <td>{row.doc.telegramUsername || "N/A"}</td>
-                                <td>{row.doc.family || "N/A"}</td>
-                                <td>{row.doc.churchContrbution || "N/A"}</td>
-                                <td>{row.doc.churchFatherName || "N/A"}</td>
-                                <td>{row.doc.churchFatherPhone || "N/A"}</td>
-                                <td>{row.doc.cheducation || "N/A"}</td>
-                                <td>{row.doc.otherchurch || "N/A"}</td>
-                                <td>{row.doc.classrepName || "N/A"}</td>
-                                <td>{row.doc.registerdate || "N/A"}</td>
+          <div className="row_posters">
+            <span className="st">የፍሬ አባላት መረጃ</span>
+            <table>
+              <thead className="title-row">
+                <tr>
+                  <th> ምስል </th>
+                  <th className="s-2">ሙሉ ስም</th>
+                  <th>የትውልድ ዝመን</th>
+                  <th className="s-3">የክርስትና ስም</th>
+                  <th> ፆታ</th>
+                  <th>የመኖሪያ አድሪሻ</th>
+                  <th className="s-4">የቤት ቁጥር</th>
+                  <th className="s-3">ዋና ስልክ ቁጥር</th>
+                  <th className="s-3">ተጨማሪ ስልክ ቁጥር</th>
+                  <th className="s-1">ኢሜል</th>
+                  <th>የትዳር ሁኔታ</th>
+                  <th>የስራ ሁኔታ</th>
+                  <th>የት/ት ደረጃ</th>
+                  <th>የተሰማሩበት የስራ ዘርፍ</th>
+                  <th>የቴሌግራም ስም</th>
+                  <th>የቤተሰብ አባላት ከስልክ ቁጥር ጋር</th>
+                  <th>የቤተክርስቲያን አገልግሎት</th>
+                  <th>የንስሐ አባት ስም</th>
+                  <th>የንስሐ አባት ስ.ቁ</th>
+                  <th>የተከታተሉት የመንፈሳዊ ት/ት</th>
+                  <th className="s-4">በሌላ ሰ/ት/ቤት ያገለገሉበት ዘርፍ</th>
+                  <th>የክፍሉ ተጠሪ ስም</th>
+                  <th>የተመዘገበበት ቀን</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((row, index) => (
+                  <tr
+                    key={row.doc._id}
+                    className={index % 2 === 0 ? "row-even" : "row-odd"}
+                  >
+                    <td>
+                      {row.doc.ProfileImage ? (
+                        <img
+                          src={URL.createObjectURL(row.doc.ProfileImage)}
+                          alt="Profile"
+                          style={{ width: 100, height: 100 }}
+                        />
+                      ) : (
+                        "N/A"
+                      )}
+                    </td>
+                    <td>{row.doc.fullName || "N/A"}</td>
+                    <td>{row.doc.Birthdate || "N/A"}</td>
+                    <td>{row.doc.churchName || "N/A"}</td>
+                    <td>{row.doc.sex || "N/A"}</td>
+                    <td>{row.doc.address || "N/A"}</td>
+                    <td>{row.doc.houseNumber || "N/A"}</td>
+                    <td>{row.doc.phone1 || "N/A"}</td>
+                    <td>{row.doc.phone2 || "N/A"}</td>
+                    <td>{row.doc.email || "N/A"}</td>
+                    <td>{row.doc.marriage || "N/A"}</td>
+                    <td>{row.doc.occupation || "N/A"}</td>
+                    <td>{row.doc.education || "N/A"}</td>
+                    <td>{row.doc.workPlace || "N/A"}</td>
+                    <td>{row.doc.telegramUsername || "N/A"}</td>
+                    <td>{row.doc.family || "N/A"}</td>
+                    <td>{row.doc.churchContrbution || "N/A"}</td>
+                    <td>{row.doc.churchFatherName || "N/A"}</td>
+                    <td>{row.doc.churchFatherPhone || "N/A"}</td>
+                    <td>{row.doc.cheducation || "N/A"}</td>
+                    <td>{row.doc.otherchurch || "N/A"}</td>
+                    <td>{row.doc.classrepName || "N/A"}</td>
+                    <td>{row.doc.registerdate || "N/A"}</td>
 
-
-                               
-
-                                <td>
-                                    <IconButton aria-label="delete" onClick={() => handleDelete(row.doc._id)}>
-                                        <DeleteIcon fontSize="inherit" />
-                                    </IconButton>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                    <td>
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => handleDelete(row.doc._id)}
+                      >
+                        <DeleteIcon fontSize="inherit" />
+                      </IconButton>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         );
 }
 
--- src/components/Mereja/MakelawianData.jsx
+++ src/components/Mereja/MakelawianData.jsx
@@ -32,84 +32,103 @@ function MakelawianData() {
     }
   };
         return (
-                <div className="row_posters">
-                    <span className='st'>የማእከላዊያን አባላት መረጃ</span>
-                <table>
-                    <thead className="title-row">
-                        <tr>
-                            <th> ምስል </th>
-                            <th className="s-2">ሙሉ ስም</th>
-                            <th>የትውልድ ዝመን</th>
-                            <th >የክርስትና ስም</th>
-                            <th> ፆታ</th>
-                            <th >የንስሐ አባት ስም</th>
-                            <th>የንስሐ አባት ስ.ቁ</th>
-                            <th >የመኖሪያ አድሪሻ</th>
-                            <th >የቤተክርስቲያን አገልግሎት</th>
-                            <th className='s-4'>የቤት ቁጥር</th>
-                            <th>የተከታተሉት የመንፈሳዊ ት/ት</th>
-                            <th className="s-3">ዋና ስልክ ቁጥር</th>
-                            <th className="s-3">ተጨማሪ ስልክ ቁጥር</th>
-                            <th className="s-1">ኢሜል</th>
-                            <th className='s-4'>በሌላ ሰ/ት/ቤት ያገለገሉበት ዘርፍ</th>
-                            <th>የት/ት ደረጃ</th>
-                            <th>የትምህርት ቤቱ ስም</th>
-                            <th >የቤተሰብ አባላት ከስልክ ቁጥር ጋር</th>
-                            <th >የተጠሪ ምስል</th>
-                            <th >የተጠሪ ስም</th>
-                            <th >የተጠሪ የቤተሰብ ሃላፊነት</th>
-                            <th >የተጠሪ የሥራ ሁኔታ</th>
-                            <th >ተጠሪ የተሰማሩበት የሥራ ዘርፍ</th>
-                            <th>የክፍሉ ተጠሪ ስም</th>
-                            <th>የተመዘገበበት ቀን</th>
-                        
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((row, index) => (
-                            <tr
-                                key={row.doc._id}
-                                className={index % 2 === 0 ? "row-even" : "row-odd"}
-                            >
-                                 <td>{row.doc.ProfileImage ? <img src={URL.createObjectURL(row.doc.ProfileImage)} alt="Profile" style={{ width: 100, height: 100 }} /> : "N/A"}</td>
-                                <td>{row.doc.fullName || "N/A"}</td>
-                                <td>{row.doc.Birthdate || "N/A"}</td>
-                                <td>{row.doc.churchName || "N/A"}</td>
-                                <td>{row.doc.sex || "N/A"}</td>
-                                <td>{row.doc.churchFatherName || "N/A"}</td>
-                                <td>{row.doc.churchFatherPhone || "N/A"}</td>
-                                <td>{row.doc.address || "N/A"}</td>
-                                <td>{row.doc.churchContrbution || "N/A"}</td>
-                                <td>{row.doc.houseNumber || "N/A"}</td>
-                                <td>{row.doc.cheducation || "N/A"}</td>
-                                <td>{row.doc.phone1 || "N/A"}</td>
-                                <td>{row.doc.phone2 || "N/A"}</td>
-                                <td>{row.doc.email || "N/A"}</td>
-                                <td>{row.doc.otherchurch || "N/A"}</td>
-                                <td>{row.doc.schoolcondition || "N/A"}</td>
-                                <td>{row.doc.schoolname || "N/A"}</td>
-                                <td>{row.doc.family || "N/A"}</td>
-                                <td>{row.doc.RespondentImage ? <img src={URL.createObjectURL(row.doc.RespondentImage)} alt="Respondent" style={{ width: 100, height: 100 }} /> : "N/A"}</td>
-                                <td>{row.doc.respondantName || "N/A"}</td>
-                                <td>{row.doc.respondantFamilyResponse || "N/A"}</td>
-                                <td>{row.doc.occupation || "N/A"}</td>
-                                <td>{row.doc.workPlace || "N/A"}</td>
-                                <td>{row.doc.classrepName || "N/A"}</td>
-                                <td>{row.doc.registerDate || "N/A"}</td>
+          <div className="row_posters">
+            <span className="st">የማእከላዊያን አባላት መረጃ</span>
+            <table>
+              <thead className="title-row">
+                <tr>
+                  <th> ምስል </th>
+                  <th className="s-2">ሙሉ ስም</th>
+                  <th>የትውልድ ዝመን</th>
+                  <th className="s-3">የክርስትና ስም</th>
+                  <th> ፆታ</th>
+                  <th className="s-3">የንስሐ አባት ስም</th>
+                  <th>የንስሐ አባት ስ.ቁ</th>
+                  <th className="s-3">የመኖሪያ አድሪሻ</th>
+                  <th>የቤተክርስቲያን አገልግሎት</th>
+                  <th className="s-4">የቤት ቁጥር</th>
+                  <th>የተከታተሉት የመንፈሳዊ ት/ት</th>
+                  <th className="s-3">ዋና ስልክ ቁጥር</th>
+                  <th className="s-3">ተጨማሪ ስልክ ቁጥር</th>
+                  <th className="s-1">ኢሜል</th>
+                  <th className="s-4">በሌላ ሰ/ት/ቤት ያገለገሉበት ዘርፍ</th>
+                  <th>የት/ት ደረጃ</th>
+                  <th>የትምህርት ቤቱ ስም</th>
+                  <th>የቤተሰብ አባላት ከስልክ ቁጥር ጋር</th>
+                  <th>የተጠሪ ምስል</th>
+                  <th className="s-3">የተጠሪ ስም</th>
+                  <th>የተጠሪ የቤተሰብ ሃላፊነት</th>
+                  <th>የተጠሪ የሥራ ሁኔታ</th>
+                  <th>ተጠሪ የተሰማሩበት የሥራ ዘርፍ</th>
+                  <th className="s-3">የክፍሉ ተጠሪ ስም</th>
+                  <th>የተመዘገበበት ቀን</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((row, index) => (
+                  <tr
+                    key={row.doc._id}
+                    className={index % 2 === 0 ? "row-even" : "row-odd"}
+                  >
+                    <td>
+                      {row.doc.ProfileImage ? (
+                        <img
+                          src={URL.createObjectURL(row.doc.ProfileImage)}
+                          alt="Profile"
+                          style={{ width: 100, height: 100 }}
+                        />
+                      ) : (
+                        "N/A"
+                      )}
+                    </td>
+                    <td>{row.doc.fullName || "N/A"}</td>
+                    <td>{row.doc.Birthdate || "N/A"}</td>
+                    <td>{row.doc.churchName || "N/A"}</td>
+                    <td>{row.doc.sex || "N/A"}</td>
+                    <td>{row.doc.churchFatherName || "N/A"}</td>
+                    <td>{row.doc.churchFatherPhone || "N/A"}</td>
+                    <td>{row.doc.address || "N/A"}</td>
+                    <td>{row.doc.churchContrbution || "N/A"}</td>
+                    <td>{row.doc.houseNumber || "N/A"}</td>
+                    <td>{row.doc.cheducation || "N/A"}</td>
+                    <td>{row.doc.phone1 || "N/A"}</td>
+                    <td>{row.doc.phone2 || "N/A"}</td>
+                    <td>{row.doc.email || "N/A"}</td>
+                    <td>{row.doc.otherchurch || "N/A"}</td>
+                    <td>{row.doc.schoolcondition || "N/A"}</td>
+                    <td>{row.doc.schoolname || "N/A"}</td>
+                    <td>{row.doc.family || "N/A"}</td>
+                    <td>
+                      {row.doc.RespondentImage ? (
+                        <img
+                          src={URL.createObjectURL(row.doc.RespondentImage)}
+                          alt="Respondent"
+                          style={{ width: 100, height: 100 }}
+                        />
+                      ) : (
+                        "N/A"
+                      )}
+                    </td>
+                    <td>{row.doc.respondantName || "N/A"}</td>
+                    <td>{row.doc.respondantFamilyResponse || "N/A"}</td>
+                    <td>{row.doc.occupation || "N/A"}</td>
+                    <td>{row.doc.workPlace || "N/A"}</td>
+                    <td>{row.doc.classrepName || "N/A"}</td>
+                    <td>{row.doc.registerDate || "N/A"}</td>
 
-
-                               
-
-                                <td>
-                                    <IconButton aria-label="delete" onClick={() => handleDelete(row.doc._id)}>
-                                        <DeleteIcon fontSize="inherit" />
-                                    </IconButton>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                    <td>
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => handleDelete(row.doc._id)}
+                      >
+                        <DeleteIcon fontSize="inherit" />
+                      </IconButton>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         );
 }
 
